31ca3120ac0a7a3912c415f070e04e0fd69ac46a
Erick Medina <erickmedinabe@gmail.com>
2020-02-17 11:45:28 +0100

    update on json

--- Model/User.php
+++ Model/User.php
@@ -11,9 +11,9 @@ $groups_json = file_get_contents('../JSON/groups.json');
 $products_json = file_get_contents('../JSON/products.json');
 
 //decoding json files into arrays
-$customer_array = json_decode($customer_json);
-$groups_array = json_decode($groups_json);
-$products_array = json_decode($products_json);
+$customer_array = json_decode($customer_json, true);
+$groups_array = json_decode($groups_json, true);
+$products_array = json_decode($products_json, true);
 
 var_dump($customer_array, $groups_array, $products_array);
 
@@ -44,15 +44,16 @@ class Group_name {
 class Products {
 
     public $name;
+    public $id;
     public $description;
     public $price;
 
 // every time an object is created, we pass the info we get from the user
-    public function __construct($name, $description, $price ) {
+    public function __construct($name, $id, $description, $price ) {
         $this->name = $name;
+        $this->id = $id;
         $this->description = $description;
         $this->price = $price;
-
     }
 
 }
--- index.php
+++ index.php
@@ -10,3 +10,58 @@ require 'Controller/HomepageController.php';
 //this file should never be more than 20 lines of code!
 $controller = new HomepageController();
 $controller->render($_GET, $_POST);
+?>
+
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- CSS links (Bootstrap) -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Price Calculator PHP</title>
+</head>
+
+<body>
+
+
+
+<div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Dropdown button
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="#">Action</a>
+        <a class="dropdown-item" href="#">Another action</a>
+        <a class="dropdown-item" href="#">Something else here</a>
+    </div>
+</div>
+
+
+<div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Dropdown button
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="#">Action</a>
+        <a class="dropdown-item" href="#">Another action</a>
+        <a class="dropdown-item" href="#">Something else here</a>
+    </div>
+</div>
+
+
+<style>
+
+</style>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
