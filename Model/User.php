5e0d836745d8d8f25741c3b1a90a13759297f249
Erick Medina <erickmedinabe@gmail.com>
2020-02-17 11:01:12 +0100

    json gotten, classes created and constructer for products made

--- Model/User.php
+++ Model/User.php
@@ -1,5 +1,23 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// getting all json files
+$customer_json = file_get_contents('../JSON/customers.json');
+$groups_json = file_get_contents('../JSON/groups.json');
+$products_json = file_get_contents('../JSON/products.json');
+
+//decoding json files into arrays
+$customer_array = json_decode($customer_json);
+$groups_array = json_decode($groups_json);
+$products_array = json_decode($products_json);
+
+var_dump($customer_array, $groups_array, $products_array);
+
+
 
 class User
 {
@@ -14,4 +32,27 @@ class User
     {
         return $this->name;
     }
+}
+
+class Customer {
+
+
+}
+class Group_name {
+
+}
+class Products {
+
+    public $name;
+    public $description;
+    public $price;
+
+// every time an object is created, we pass the info we get from the user
+    public function __construct($name, $description, $price ) {
+        $this->name = $name;
+        $this->description = $description;
+        $this->price = $price;
+
+    }
+
 }
