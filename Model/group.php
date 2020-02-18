29e9e82bc8fcbec855f576b3d3423c17aeeb5844
Erick Medina <erickmedinabe@gmail.com>
2020-02-18 10:31:37 +0100

    added class group

--- Model/group.php
+++ Model/group.php
@@ -5,11 +5,21 @@ $groups_json = file_get_contents('JSON/groups.json');
 
 //decoding json files into arrays
 $groups_array = json_decode($groups_json);
-//var_dump($customer_array, $groups_array, $products_array);
-
+//var_dump($groups_array);
 
 
 class Group
 {
+    private $id;
+    private $name;
+    private $variable_discount;
+    private $group_id;
+
+    public function __construct($id, $name, $variable_discount, $group_id ) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->variable_discount = $variable_discount;
+        $this->group_id = $group_id;
+    }
 
 }
